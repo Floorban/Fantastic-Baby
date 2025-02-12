0b7e9555ca46bce1b5142ce1c0a03fed54a853f1
Eric Li <15696082812@163.com>
2025-02-12 23:50:03 +0100

    spawn obj works

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -3,31 +3,73 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    public AnimationCurve curve;
-    [SerializeField] private float duration;
-    [SerializeField] private float maxHeightY;
-
-    public Transform target;
-    private void Start()
+    public bool isPlaying = true;
+    [SerializeField] Transform spawnArea;
+    [SerializeField] Sprite[] objSprites;
+    public GameObject spawnedObj;
+    public float minSpawnTime;
+    public float maxSpawnTime;
+    public float spawnRadius, innerRadius;
+    void Start()
     {
-        StartCoroutine(Curve(transform.position, target.position));
+        StartCoroutine(SpawnObjs());
     }
-    public IEnumerator Curve(Vector3 start, Vector3 finish)
+    IEnumerator SpawnObjs()
     {
-        var timePast = 0f;
-
-        while (timePast < duration)
+        while (isPlaying)
         {
-            timePast += Time.deltaTime;
-
-            var linearTime = timePast / duration;
-            var heightTime = curve.Evaluate(linearTime);
-
-            var height = Mathf.Lerp(0f, maxHeightY, heightTime); //clamped between the max height and 0
-
-            transform.position = Vector3.Lerp(start, finish, linearTime) + new Vector3(0f, height, 0f);
-
-            yield return null;
+            SpawnObj();
+            float randomInterval = Random.Range(minSpawnTime, maxSpawnTime);
+            yield return new WaitForSeconds(randomInterval);
         }
+    }
+
+    void SpawnObj()
+    {
+        GameObject ga = Instantiate(spawnedObj, transform.position + Vector3.up, spawnedObj.transform.rotation);
+        ga.transform.SetParent(spawnArea);
+        SpawnedObj obj = ga.GetComponent<SpawnedObj>();
+
+        if (obj != null)
+        {
+            Sprite randomSprite = objSprites[Random.Range(0, objSprites.Length)];
+            obj.Init(randomSprite, GetRandomPosition());
+        }
+    }
+
+    Vector3 GetRandomPosition()
+    {
+        Vector3 spawnPos = Vector3.zero;
+        bool validPosition = false;
+
+        while (!validPosition)
+        {
+            float angle = Random.Range(0f, 2f * Mathf.PI); 
+            float radius = Random.Range(innerRadius, spawnRadius);  
+
+            float x = Mathf.Cos(angle) * radius;
+            float y = Mathf.Sin(angle) * radius;
+
+            spawnPos = new Vector3(spawnArea.position.x + x, spawnArea.position.y, spawnArea.position.z + y);
+
+            float distanceToCenter = Vector3.Distance(spawnArea.position, spawnPos);
+            if (distanceToCenter >= innerRadius)
+            {
+                validPosition = true;  
+            }
+        }
+
+        return spawnPos;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+
+        Vector3 spawnPos = spawnArea.position;
+        Gizmos.DrawWireSphere(spawnPos, spawnRadius);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(spawnPos, innerRadius); 
     }
 }
---
+++ Assets/Scripts/SpawnedObj.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System.Collections;
+
+public class SpawnedObj : MonoBehaviour
+{
+    Camera cam;
+    [Header("Properties")]
+    private SpriteRenderer sr;
+
+    [Header("Throw Anim")]
+    public AnimationCurve curve;
+    public float duration;
+    public float maxHeightY;
+    private void Awake()
+    {
+        cam = FindFirstObjectByType<Camera>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+    private void LateUpdate()
+    {
+        if (cam != null)
+        {
+            transform.LookAt(cam.transform.position);
+        }
+    }
+    public void Init(Sprite image, Vector3 targetPos)
+    {
+        if (targetPos == null || image == null) return;
+        sr.sprite = image;
+        StartCoroutine(Curve(transform.position, targetPos));
+    }
+    private IEnumerator Curve(Vector3 start, Vector3 finish)
+    {
+        var timePast = 0f;
+
+        while (timePast < duration)
+        {
+            timePast += Time.deltaTime;
+
+            var linearTime = timePast / duration;
+            var heightTime = curve.Evaluate(linearTime);
+
+            var height = Mathf.Lerp(0f, maxHeightY, heightTime); //clamped between the max height and 0
+
+            transform.position = Vector3.Lerp(start, finish, linearTime) + new Vector3(0f, height, 0f);
+
+            yield return null;
+        }
+    }
+}
